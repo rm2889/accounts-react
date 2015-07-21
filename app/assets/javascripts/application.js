7f79e23cc3242e247f769658544dbd7a8d2a38df
Raghav Malik <raghav1987@gmail.com>
2015-07-21 20:24:47 +0530

    simple table display with two components

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,11 @@
 // about supported directives.
 //
 //= require jquery
+//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
+//= require react
+//= require react_ujs
+//= require components
 //= require_tree .
+
